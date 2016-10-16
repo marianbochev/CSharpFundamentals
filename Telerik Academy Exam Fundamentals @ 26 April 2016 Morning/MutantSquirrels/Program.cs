7677cb544a9c02c9672cef5c89bec10bf7796e82
marianbochev <marian_bochev@hotmail.com>
2016-10-16 17:00:25 +0300

    Telerik Exam 26 april Morning - Practice

---
+++ Telerik Academy Exam Fundamentals @ 26 April 2016 Morning/MutantSquirrels/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MutantSquirrels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//input
+            ulong t = ulong.Parse(Console.ReadLine());
+            ulong b = ulong.Parse(Console.ReadLine());
+            ulong s = ulong.Parse(Console.ReadLine());
+            ulong n = ulong.Parse(Console.ReadLine());
+
+            //logic
+            double totalTails = t * b * s * n;
+            if (totalTails % 2 == 0)
+            {
+                totalTails = totalTails * 376439;
+                Console.WriteLine("{0:F3}", totalTails);
+            }
+            else
+            {
+                totalTails = totalTails / 7;
+                Console.WriteLine("{0:F3}", totalTails);
+            }
+
+        }
+    }
+}
