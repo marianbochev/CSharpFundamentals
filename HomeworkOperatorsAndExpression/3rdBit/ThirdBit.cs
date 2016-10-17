15387148c42fee884b0906c370f74eb9382327c2
marianbochev <marian_bochev@hotmail.com>
2016-10-18 02:21:31 +0300

    HomeWork

---
+++ HomeworkOperatorsAndExpression/3rdBit/ThirdBit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ThirdBit
+{
+    class ThirdBit
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            string binary = Convert.ToString(num, 2);
+            Console.WriteLine(binary);
+
+
+
+
+        }
+    }
+}
---
+++ HomeworkOperatorsAndExpression/PrimeNumbers2/PrimeNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+    class PrimeNumbers
+    {
+        static void Main()
+        {
+            sbyte num = sbyte.Parse(Console.ReadLine());
+            int remainder = 0;
+            string result = "true";
+            if (num > 1)
+            {
+                for (sbyte i = 2; i <num; i++)
+                {
+                    remainder = num % i;
+                    if (remainder == 0)
+                    {
+                        result = "false";
+                        break; 
+                    }
+                }
+            }
+            // if num is < 2
+            else
+            {
+                result = "false";
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
