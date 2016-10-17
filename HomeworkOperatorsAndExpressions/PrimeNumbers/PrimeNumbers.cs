15e884c3384bbbf60142154c8b12964af64bf921
marianbochev <marian_bochev@hotmail.com>
2016-10-18 02:16:22 +0300

    Homework

---
+++ HomeworkOperatorsAndExpressions/PrimeNumbers/PrimeNumbers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+    class PrimeNumbers
+    {
+        static void Main()
+        {
+            //input
+            int num = int.Parse(Console.ReadLine());
+           
+        }
+    }
+}
